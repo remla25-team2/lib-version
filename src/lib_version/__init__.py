95bf7ca9b0e211ea803e1cd2f94f1cb71155da17
Angelos <nikosmimo@gmail.com>
2025-05-04 15:01:44 +0200

    Fixed version mismatch

--- src/lib_version/__init__.py
+++ src/lib_version/__init__.py
@@ -1,9 +1,3 @@
-import os
-import json
+from version_util import VersionUtil
 
-# Try to get version from version.json
-try:
-    with open(os.path.join(os.path.dirname(__file__), "version.json")) as f:
-        __version__ = json.load(f)["version"]
-except (FileNotFoundError, KeyError):
-    __version__ = "0.0.0"
+__version__ = VersionUtil.get_version()
