62644bf7aa4f5a0ba8f6c13c180f43670517e136
Angelos <nikosmimo@gmail.com>
2025-05-07 13:37:10 +0200

    Remove handling for original major.minor tags in build process and streamline version determination logic

--- src/lib_version/builder.py
+++ src/lib_version/builder.py
@@ -92,11 +92,6 @@ class PackageBuilder:
         try:
             # Save the original tag if it's a vX.Y format
             original_tag = VersionUtil.get_latest_tag()
-            is_major_minor_tag = False
-            if original_tag:
-                parsed = VersionUtil.parse_version(original_tag)
-                if parsed and parsed[2] is None:  # No patch specified (vX.Y format)
-                    is_major_minor_tag = True
             
             # Determine version and whether a new tag was created
             version, created_tag = self.determine_version()
@@ -133,11 +128,6 @@ class PackageBuilder:
             
             # Print the output of the build command
             print(result.stdout.decode())
-            
-            # Delete the original vX.Y tag if a new vX.Y.Z tag was created
-            if is_major_minor_tag and created_tag:
-                print(f"Deleting major.minor tag {original_tag} after creating {version}")
-                VersionUtil.delete_tag(original_tag, push=True)
 
             # delete all major.minor tags
             major_minor_tags = VersionUtil.get_major_minor_tags()
